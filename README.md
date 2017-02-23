d06021d71271b595c39932944e337f3f9eca76eb
Rai Phairow <rai.phairow@github.com>
2017-02-22 22:56:44 -0800

    initialize

---
+++ README.md
@@ -0,0 +1,5 @@
+# Installation
+> `npm install --save @types/loopback`
+
+# Summary
+This package contains type definitions for Loopback (https://loopback.io/).
---
+++ index.d.ts
@@ -0,0 +1,26 @@
+/// <reference types="express-serve-static-core" />
+
+// Type definitions for Loopback
+
+/* =================== USAGE ===================
+
+    import * as loopback from "loopback";
+    const app: loopback.Loopback = loopback();
+
+ =============================================== */
+
+import * as core from "express-serve-static-core";
+
+export = loopback;
+
+declare global {
+
+  function loopback(): loopback.Loopback;
+
+  namespace loopback {
+    export interface Loopback extends core.Express {
+      start():void;
+      emit(message: string):void;
+    }
+  }
+}
